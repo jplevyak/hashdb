6e2949a926e3e463d5d2549cc759f17283a79402
John Plevyak <jplevyak@gmail.com>
2026-01-28 22:17:46 -0800

    Update initialization

--- hdb.h
+++ hdb.h
@@ -14,20 +14,20 @@ class HDB : public HashDB {
   std::mutex mutex;
   std::thread sync_thread;
   std::condition_variable sync_condition;
-  int exiting = 0;
-  int read_only = 0;
+  int exiting{0};
+  int read_only{0};
 
   int foreach_slice(void *(*pfn)(Doer *));
   void free_slices();
 
   uint32_t thread_pool_allocated : 1 = 0;
-  int thread_pool_max_threads = std::numeric_limits<int>::max();
-  int current_write_slice = 0;
+  int thread_pool_max_threads{std::numeric_limits<int>::max()};
+  int current_write_slice{0};
 
   // FUTURE Config variables
-  int init_generations = 1;
-  bool separate_db_per_slice = true;
-  int replication_factor = 0;
+  int init_generations{1};
+  bool separate_db_per_slice{true};
+  int replication_factor{0};
 
   int warn(cchar *format, ...);
   int err(cchar *format, ...);
